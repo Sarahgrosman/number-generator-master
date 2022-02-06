6d0efd16c6a8d2c95523d5f964211ff8aa781aea
Sarahgrosman <sarahg@webschool.co.il>
2022-02-06 22:19:18 +0200

    init project random number with state

---
+++ src/components/Square.js
@@ -0,0 +1,54 @@
+import react, { useState } from "react"
+
+const isEven = (number) => {
+    return number % 2 == 0;
+}
+
+const isPrime = (number) => {
+    for (let i = 2; i <= number; i++) {
+        if (i != number && number % i == 0)
+            return false;
+    }
+    if (number == 0 || number == 1)
+        return false;
+    return true;
+}
+
+
+const classNameSquare = (number) => {
+    let className = 'square';
+    if (isPrime(number))
+        className += ' prime';
+    else {
+        if (isEven(number))
+            className += ' even';
+        else
+            className += ' odd';
+    }
+    return className;
+}
+
+
+const Square = ({number}) => {
+  const [stateSquare,setStateSquare]=useState(number)
+  console.log(number)
+  const randomNumbers = () => {
+    setStateSquare((Math.round (Math.random()*100))+parseInt(stateSquare))
+    
+  }
+  return (
+    <div className={classNameSquare(stateSquare)}>
+        <button className="number" onClick={randomNumbers}>{stateSquare}</button>
+
+    </div>
+);
+};
+
+
+
+
+
+export default Square;
+
+
+
